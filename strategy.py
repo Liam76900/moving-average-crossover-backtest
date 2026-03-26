ee6e666bb3711a0e3c88d7e103869ac81f2b6d69
Liam76900 <ljthomson@gmail.com>
2026-03-26 01:17:22 +0000

    Added short and long moving averages

--- strategy.py
+++ strategy.py
@@ -5,3 +5,7 @@ from dataloader import load_data
 def create_signals(data):
 
     data["Return"]=data["Close"].pct_change()
+
+    short_ma=data["Close"].rolling(10).mean()
+
+    long_ma=data["Close"].rolling(50).mean()
