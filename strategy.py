4f0bb84c980d82d125261b78e46e8865207198ef
Liam76900 <ljthomson@gmail.com>
2026-03-26 01:21:51 +0000

    Added the ability to change the window in the function

--- strategy.py
+++ strategy.py
@@ -2,13 +2,13 @@ import yfinance as yf
 
 from dataloader import load_data
 
-def create_signals(data):
+def create_signals(data, short_window, long_window):
 
     data["Return"]=data["Close"].pct_change()
 
-    short_ma=data["Close"].rolling(10).mean()
+    short_ma=data["Close"].rolling(short_window).mean()
 
-    long_ma=data["Close"].rolling(50).mean()
+    long_ma=data["Close"].rolling(long_window).mean()
 
     data["Signal"] = 0
     data.loc[short_ma > long_ma, "Signal"] = 1
