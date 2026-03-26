2ca9970ba603cb98e741d93c6725ae4c981544c4
Liam76900 <ljthomson@gmail.com>
2026-03-26 01:41:39 +0000

    Added risk metrics to main code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dataloader import load_data
 from strategy import create_signals
 from backtest import backtesting
+from metrics import calculate_metrics
 
 windows=[(5,20),(10,50),(20,100)]
 symbols=["AAPL","MSFT","GOOGL"]
@@ -14,3 +15,10 @@ for short_window,long_window in windows:
 
         print(f"Results for {symbol} for {short_window} day short window and {long_window} day long window is")
         print(data[["Cumulative_Market", "Cumulative_Strategy"]].tail())
+        sharpe_ratio, annualised_volatility, max_drawdown=calculate_metrics(data)
+        print("Sharpe ratio:")
+        print(sharpe_ratio)
+        print("Annualised Volatility:")
+        print(annualised_volatility)
+        print("Max Drawdown:")
+        print(max_drawdown)
