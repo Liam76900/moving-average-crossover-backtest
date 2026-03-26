12c368874f3611b6432d439f2de065fe77e3abcf
Liam76900 <ljthomson@gmail.com>
2026-03-26 01:27:16 +0000

    Added method to calculate strategy returns using signals, cumulative market returns and cumulative strategy returns

--- backtest.py
+++ backtest.py
@@ -1,2 +1,7 @@
 def backtesting(data):
     data["Strategy_Returns"]=data["Signal"] * data["Return"]
+
+    data["Cumulative_Market"]=(1+data["Return"]).cumprod()
+    data["Cumulative_Strategy"]=(1+data["Strategy_Returns"]).cumprod()
+
+    return data
