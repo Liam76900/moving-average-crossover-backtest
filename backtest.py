aba6ac78f09288e90e22fd4bf2ecfc307721e583
Liam76900 <ljthomson@gmail.com>
2026-03-27 21:42:05 +0000

    Fixed transaction costs system

--- backtest.py
+++ backtest.py
@@ -9,8 +9,8 @@ def backtesting(data):
     data["Net_Returns"]=data["Strategy_Returns"]-data["Transaction Costs"]
 
     data["Market_Transaction_Costs"]=0
-    data.iloc(0, data.columns.get_loc("Market_Transaction_Costs")) = cost
-    data.iloc(-1, data.columns.get_loc("Market_Transaction_Costs")) = cost
+    data.iloc(0, data.columns.get_loc("Market_Transaction_Costs")) == cost
+    data.iloc(-1, data.columns.get_loc("Market_Transaction_Costs")) == cost
     data["Net_Market_Returns"]=data["Returns"] - data["Market_Transaction_Costs"]
     data["Cumulative_Market"]=(1+data["Return"]).cumprod()
     data["Cumulative_Strategy"]=(1+data["Net Returns"]).cumprod()
