7d8c1c06d215c066f08e3618d94502d8f637127a
Liam76900 <ljthomson@gmail.com>
2026-03-28 00:32:04 +0000

    Added comments and fixed layout of main code

--- main.py
+++ main.py
@@ -4,29 +4,50 @@ from strategy import create_signals
 from backtest import backtesting
 from metrics import calculate_metrics
 
+#Assigns the different parameters for the windows of time and the different assets
+
 windows=[(5,20),(10,50),(20,100)]
-symbols=["AAPL","MSFT","GOOGL"]
+assets=["AAPL","MSFT","GOOGL"]
+
+#Loops through the different parameters for the windows of time
 
 for short_window,long_window in windows:
 
+    #Creates a new figure for our plot
+
     plt.figure(figsize=(10, 5))
 
-    for symbol in symbols:
+    #Loops through the different assets
 
-        data=load_data(symbol)
+    for asset in assets:
+
+        #Assigned variables are ran through the different defined functions
+
+        data=load_data(asset)
         data=create_signals(data, short_window, long_window)
         data=backtesting(data)
-
-        print(f"Results for {symbol} for {short_window} day short window and {long_window} day long window is")
-        print(data[["Cumulative_Market", "Cumulative_Strategy"]].tail())
         sharpe_ratio, annualised_volatility, max_drawdown=calculate_metrics(data)
+
+        #Outputs the dataframe for Cumulative Market and Strategy Return values
+
+        print(f"Results for {asset} for {short_window} day short window and {long_window} day long window is")
+        print(data[["Cumulative_Market", "Cumulative_Strategy"]].tail())
+
+        #Outputs Risk Metrics
+
         print("Sharpe ratio:")
         print(sharpe_ratio)
         print("Annualised Volatility:")
         print(annualised_volatility)
         print("Max Drawdown:")
         print(max_drawdown)
-        plt.plot(data["Cumulative_Strategy"], label=symbol)
+
+        #Plots Cumulative Strategy Returns of all the different asssets over the time frame
+
+        plt.plot(data["Cumulative_Strategy"], label=asset)
+
+    #Adds legend, axis labels, title, gridlines to graph and returns the graph
+
     plt.legend()
     plt.xlabel("Time")
     plt.ylabel("Returns")
@@ -34,19 +55,30 @@ for short_window,long_window in windows:
     plt.grid(True, linestyle="--", alpha=0.5)
     plt.show()
 
-    plt.figure(figsize=(10, 5))
-    for symbol in symbols:
+    #Creates a new figure for our plot
 
-        data=load_data(symbol)
+    plt.figure(figsize=(10, 5))
+
+    #Loops through different assets
+
+    for asset in assets:
+
+        #Assigned variables are ran through the different defined functions
+
+        data=load_data(asset)
         data=create_signals(data, short_window, long_window)
         data=backtesting(data)
         sharpe_ratio, annualised_volatility, max_drawdown=calculate_metrics(data)
+
+        #Plots graph of drawdown against time
         
-        plt.plot(data["Drawdown"], label=symbol)
-    plt.title("Drawdown Over Time")
+        plt.plot(data["Drawdown"], label=asset)
+
+    #Adds legend, axis labels, title, gridlines to graph and returns the graph
+
     plt.legend()
-    plt.grid(True, linestyle="--", alpha=0.5)
     plt.xlabel("Time")
     plt.ylabel("Drawdown")
+    plt.title("Drawdown Over Time")
     plt.grid(True, linestyle="--", alpha=0.5)
     plt.show()
