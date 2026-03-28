537b5b7b7abbdc7eede48743ec26ab532359f7de
Liam76900 <ljthomson@gmail.com>
2026-03-28 01:12:11 +0000

    Added comments

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 
-def load_data(symbol):
-    data = yf.download(symbol, start="2023-01-01", end="2024-01-01")
+#Loads real stock data of different assets from yfinance
+
+def load_data(asset):
+    data = yf.download(asset, start="2023-01-01", end="2024-01-01")
     data.columns = data.columns.droplevel(1)
     return data
